baaf6c526c06fbd2e3c69e669460892c82ee712c
dmpeters <dmpeters63@gmail.com>
2012-08-18 08:36:35 -0700

    working through facebook

--- pcloud/repos/facebook/photos.py
+++ pcloud/repos/facebook/photos.py
@@ -1,4 +1,5 @@
 import requests
+import pdb
 
 class DjangoFacebookPhotos(object):
 	
@@ -6,21 +7,25 @@ class DjangoFacebookPhotos(object):
 		self._app_id = '230178707104819'
 		self._app_secret = '7fc59c3c5a7800804b73e9d2c1006d05'
 		self._app_redirect_url = 'http://127.0.0.1:8000'
-		self._app_scope = 'user_photos'
-		self._app_state = 'u942146p6o91u9'
+		self._app_scope = 'id, user_photos'
 	
 	def access_token(self):
-		url =	'https://www.facebook.com/dialog/oauth?\
+		url = 'https://www.facebook.com/dialog/oauth?\
 				client_id={}&\
 				redirect_uri={}&\
 				scope={}&\
-				state={}'.format(self._app_id, self._app_redirect_url, self._app_scope, self._app_state)
-		
+				state={}'.format(self._app_id, self._app_redirect_url, self._app_scope)
 		r = requests.get(url)
-		return r.text.split('=')[1];
 	
-	def getAlbums(self):
-	
-	def getPhotos(self):
+	def get_albums(self, id, token):
+		table =  'album'
+		query = 'https://graph.facebook.com/fql?\
+					q=SELECT+aid+FROM+{}+WHERE+\
+					uid1=me()&{}'.format(album, code)
+		q = requests.get(query)
+		
+		# for aid in q:
+		
+	def get_photos(self, id, albums):
 	
 	
