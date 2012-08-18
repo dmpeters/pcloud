6dee95723f78e3a15194f1f394ebf059145d61f5
Dino Petrone <dinopetrone@gmail.com>
2012-08-18 16:31:37 -0700

    fixing class

--- pcloud/repos/facebook/photos.py
+++ pcloud/repos/facebook/photos.py
@@ -2,20 +2,22 @@ import requests, json, pdb
 
 class Facebook(object):
 	
-	def __init__(self, code=None):
-		app_id = '230178707104819'
-		app_secret = '7fc59c3c5a7800804b73e9d2c1006d05'
-		app_redirect_url = 'http://127.0.0.1:8000'
-		app_scope = 'id, user_photos'
-		token = token
+	def __init__(self, token=None):
+		self.app_id = '230178707104819'
+		self.app_secret = '7fc59c3c5a7800804b73e9d2c1006d05'
+		self.app_redirect_url = 'http://127.0.0.1:8000'
+		self.app_scope = 'id, user_photos'
+		self.token = token
 		self.get_photos()
 	
-	def get_photos(self, id, albums):
+	def get_photos(self):
 		
 		# Get Photos
 		query = 'https://graph.facebook.com/fql?q=\
 				SELECT src_big FROM photo WHERE aid IN (\
 				SELECT aid FROM album WHERE owner=me()\
-				&access_token={})'.format(token)
+				&access_token={})'.format(self.token)
 		q = requests.get(query)
-		photos = q
+		print q
+		photos = q.json
+		print photos
