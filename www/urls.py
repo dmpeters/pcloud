8acf997c3a3f49caa3d383c054d066681de2e2f5
Dino Petrone <dinopetrone@gmail.com>
2012-08-17 22:13:44 -0700

    functioning app

--- www/urls.py
+++ www/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from www.views.index import IndexView
-from tastypie.api import Api
-from win8core.api.messages import MessageResource, ShowResource, ShowSchedule, WordCloud
 from www import settings
 
-v1_api = Api(api_name='v1')
-v1_api.register(MessageResource())
-v1_api.register(ShowResource())
-v1_api.register(ShowSchedule())
-v1_api.register(WordCloud())
 
 admin.autodiscover()
 
@@ -18,7 +11,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'www.views.home', name='home'),
     # url(r'^www/', include('www.foo.urls')),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    (r'^api/', include(v1_api.urls)),
     (r'^grappelli/', include('grappelli.urls')),
     (r'^resources/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'public/resources'}),
     url(r'^admin/', include(admin.site.urls)),
